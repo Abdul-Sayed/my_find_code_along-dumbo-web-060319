24e56eb168f5d6f750042f08f0dde07c03d30eea
Abdul <awsayed@alaska.edu>
2019-06-04 22:26:07 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,7 @@ def my_find(collection)
   i = 0
   while i < collection.length
     found = yield(collection[i])
-    return collection[i] if found == true
+    return collection[i] if found
     i += 1
   end
 end
